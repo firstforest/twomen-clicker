817e6c178e65f555915f91a9615e454ab021a6e1
Hajime <h.morikosi@gmail.com>
2023-08-06 02:13:43 +0900

    キャラクターを追加します

---
+++ scenes/aoi/aoi.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+@onready var animation_player = $AnimationPlayer
+@onready var sprite = $Sprite2D
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var _direction = -1
+
+
+func _ready() -> void:
+	animation_player.play("walk")
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	if is_on_floor():
+		velocity.x = _direction * SPEED
+
+	if position.x < 0:
+		_direction = 1
+		sprite.flip_h = true
+	elif position.x > 1200:
+		_direction = -1
+		sprite.flip_h = false
+
+	move_and_slide()
