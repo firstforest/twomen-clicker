f3185c048d14d0b9431188e827cc504e7aafc14a
Hajime <h.morikosi@gmail.com>
2023-08-06 09:25:05 +0900

    おにぎりを食べた時のSEを追加します

--- scenes/onigiri/onigiri.gd
+++ scenes/onigiri/onigiri.gd
@@ -1,12 +1,15 @@
 extends RigidBody2D
 class_name Onigiri
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass  # Replace with function body.
+@export var eaten_se: AudioStream
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			eaten()
+
+
+func eaten() -> void:
+	AudioManager.play(eaten_se.resource_path)
+	queue_free()
